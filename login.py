76dafe8c65bf631f750ee400cb86914f9f3e10e8
KOTHAPALLIRAJASEKHARREDDY <rajasekharreddy752@gmail.com>
2024-11-25 02:52:08 -0600

    Minor chages

--- login.py
+++ login.py
@@ -18,11 +18,24 @@ def user_login(collection):
     password = request.form.get('password', '').strip()
 
     if username and password:
+<<<<<<< Updated upstream
         # if username.lower() == 'admin' and password == 'Admin.123':
         #     return True, admin
         # else:
         isLogin, user = check_for_user_credentials(
             username, password, collection)
+=======
+        if username.lower() == 'admin' and password == 'Admin.123':
+            return "Admin"
+        else:
+            isLogin = check_for_user_credentials(username, password, collection)
+            if isLogin:
+                print("redirecting to dashboard")
+                return "true"
+            else:
+                return "false"
+    return "false"
+>>>>>>> Stashed changes
 
         return isLogin, user
         # if isLogin:
@@ -38,6 +51,7 @@ def check_for_user_credentials(username, password, collection):
     user = collection.find_one({'email': username})
     if user and user['password'] == password:
         print("returning true")
+<<<<<<< Updated upstream
         user_data = {
             'user_id': str(user['_id']),
             'username': user['email'],
@@ -48,3 +62,8 @@ def check_for_user_credentials(username, password, collection):
         return True, user_data
     print("returning flase")
     return False, None
+=======
+        return True
+    return False
+
+>>>>>>> Stashed changes
